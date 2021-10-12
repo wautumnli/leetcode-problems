8f7a8a3076d202647e2219a380d287ecc1a7e740
万秋里 <wanqiuli@jd.com>
2021-10-12 19:39:31 +0800

    LeetCode 83

---
+++ leetcode-solve/src/main/java/com/ql/leetcode/Leetcode83.java
@@ -0,0 +1,28 @@
+package com.ql.leetcode;
+
+public class Leetcode83 {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        int first = head.val;
+        while (head != null) {
+
+        }
+    }
+}
