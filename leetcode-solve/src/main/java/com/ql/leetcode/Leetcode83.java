9fd525a0334c2b3b19ab55e38f03de2964a473b0
万秋里 <wanqiuli@jd.com>
2021-10-13 13:38:42 +0800

    LeetCode 83

--- leetcode-solve/src/main/java/com/ql/leetcode/Leetcode83.java
+++ leetcode-solve/src/main/java/com/ql/leetcode/Leetcode83.java
@@ -19,10 +19,33 @@ public class Leetcode83 {
         }
     }
 
-    public ListNode deleteDuplicates(ListNode head) {
-        int first = head.val;
-        while (head != null) {
-
+    public static void main(String[] args) {
+        ListNode b = new ListNode(3, null);
+        ListNode a = new ListNode(3, b);
+        ListNode one = new ListNode(2, a);
+        ListNode two = new ListNode(1, one);
+        ListNode three = new ListNode(1, two);
+        ListNode listNode = new Leetcode83().deleteDuplicates(three);
+        while (listNode != null) {
+            System.out.println(listNode.val);
+            listNode = listNode.next;
         }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+        ListNode aux = head;
+        ListNode res = head;
+        while (head != null) {
+            if (res.val != head.val) {
+                res.next = head;
+                res = head;
+            }
+            if (head.next == null) {
+                res.next = null;
+            }
+            head = head.next;
+        }
+        return aux;
     }
 }
