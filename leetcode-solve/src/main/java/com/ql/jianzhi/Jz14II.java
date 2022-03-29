d6b96ff5b824c0c42f161b3786f45690e1797ba7
wanqiuli <wanqiuli@jd.com>
2022-03-29 13:04:49 +0800

    【add】Jz14II

---
+++ leetcode-solve/src/main/java/com/ql/jianzhi/Jz14II.java
@@ -0,0 +1,29 @@
+package com.ql.jianzhi;
+
+/**
+ * @author wanqiuli
+ * @date 2022/3/29 12:35 下午
+ */
+public class Jz14II {
+    public int cuttingRope(int n) {
+        // todo 快速幂求余
+        if (n <= 3) {
+            return n - 1;
+        }
+        int b = n % 3, p = 1000000007;
+        long rem = 1, x = 3;
+        for (int a = n / 3 - 1; a > 0; a /= 2) {
+            if (a % 2 == 1) {
+                rem = (rem * x) % p;
+            }
+            x = (x * x) % p;
+        }
+        if (b == 0) {
+            return (int) (rem * 3 % p);
+        }
+        if (b == 1) {
+            return (int) (rem * 4 % p);
+        }
+        return (int) (rem * 6 % p);
+    }
+}
