51060b7f09cdc87de6e99599007f83802a45354f
wanqiuli <wanqiuli@jd.com>
2022-03-31 20:31:20 +0800

    【add】Jz50

---
+++ leetcode-solve/src/main/java/com/ql/jianzhi/Jz50.java
@@ -0,0 +1,24 @@
+package com.ql.jianzhi;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author wanqiuli
+ * @date 2022/3/31 20:21
+ */
+public class Jz50 {
+
+    public char firstUniqChar(String s) {
+        int[] array = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            array[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (array[s.charAt(i) - 'a'] == 1) {
+                return s.charAt(i);
+            }
+        }
+        return ' ';
+    }
+}
